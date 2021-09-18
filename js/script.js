b4f25d716781265841e6e7e338838317383d91e4
Steve Hoskins <80352473+stevehoskins@users.noreply.github.com>
2021-09-18 10:20:08 +0100

    working on nod js - start - udemy

--- js/script.js
+++ js/script.js
@@ -4,7 +4,26 @@ const numbers = [4, 9, 16, 25];
 numbersSqrt = numbers.map(Math.sqrt);
 numbersSquared = numbers.map(numbers => numbers * numbers);
 
+/**************************************************************/
+
 document.getElementById("demo0").innerHTML = numbers;
 document.getElementById("demo1").innerHTML = numbersSqrt; document.getElementById("demo2").innerHTML = numbersSquared;
 document.getElementById("demo3").innerHTML = numbers.map(numbers => numbers  + 10);
 document.getElementById("demo4").innerHTML = numbers.map(numbers => ((numbers  + 10) * (numbers + 10)));
+
+/**************************************************************/
+
+
+/*
+function v(val)  
+	{
+		document.getElementById("d").value+=val;  
+	} 
+*/
+
+/*
+function v(val)  
+	{
+		document.getElementById("d").value+=val;  
+	} 
+*/
